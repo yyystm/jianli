6edfe9f705405e056ea5b99cafdd11a403304f55
NNU_YYY <21230213@njnu.edu.cn>
2026-05-03 19:00:52 +0800

    Deploy academic frontend refinements

--- assets/js/cookie-consent-setup.js
+++ assets/js/cookie-consent-setup.js
@@ -96,7 +96,7 @@ function initializeCookieConsent() {
               },
               {
                 title: 'More information',
-                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://yyystm.github.io/jianli/#contact">contact us</a>.'
+                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="http://localhost:8080/#contact">contact us</a>.'
               }
             ]
           }
