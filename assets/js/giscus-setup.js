8b808aab5f33de9183e2eafa22afc0ee98f498dc
yyystm <21230213@njnu.edu.cn>
2026-05-14 10:22:00 +0000

    Deploying to gh-pages from @ yyystm/jianli@67bf0b26d00386e5887cbac9499c523e946f2f72 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "",
+    "data-repo": "yyystm/jianli",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
